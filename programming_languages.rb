c4a0315d8eb0933878cebfa387287ee45c605b19
Chett Tiller <chett.tiller@gmail.com>
2018-09-25 12:11:27 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |key, value| # key = oo / value = big block
     value.each do |lang,v|# lang = ruby / v = type=>"interpreted"
-      new_hash[lang] = 
+      new_hash[lang] = v
     end
   end
 end
