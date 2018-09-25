53e09be7842fdae6c49d13067bb7e09c10ecbc58
Chett Tiller <chett.tiller@gmail.com>
2018-09-25 12:01:06 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
 
 end
+
+#languages = {
+#  :oo => {
+#    :ruby => {:type => "interpreted"},
+#    :javascript => {:type => "interpreted"},
+#    :python => {:type => "interpreted"},
+#    :java => {:type => "compiled"}
+#  },
+#  :functional => {
+#    :clojure => {:type => "compiled"},
+#    :erlang => {:type => "compiled"},
+#    :scala => {:type => "compiled"},
+#    :javascript => {:type => "interpreted"} 
+#  }
+#}
